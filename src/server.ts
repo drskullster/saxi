e29455f5e1758c4818dca3f895f7cd1a98dffaad
Jonathan Berger <drskullster@pm.me>
2025-02-21 21:30:25 +0100

    Change microstepping resolution to avoid offsets on iDraw H SE

--- src/server.ts
+++ src/server.ts
@@ -176,7 +176,7 @@ export async function startServer (port: number, hardware: Hardware = 'v3', com:
 
   const realPlotter: Plotter = {
     async prePlot(initialPenHeight: number): Promise<void> {
-      await ebb.enableMotors(1); // 16x microstepping, matches defaults from Axidraw
+      await ebb.enableMotors(1);
       await ebb.setPenHeight(initialPenHeight, 1000, 1000);
     },
     async executeMotion(motion: Motion, _progress: [number, number]): Promise<void> {
